21655303d14a740f56f56f7c8176561fd5aa81c7
brandon-m-navarro <brandon.m.navarro@gmail.com>
2024-09-28 15:25:48 -0400

    Fixed font-sizing issues of Brandon Inc. text. Changed text for landing page

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Brandon Inc.</p>
+      <GlobeAltIcon className="min-h-5 max-h-7 min-w-5 rotate-[15deg]" />
+      <p className="text-xl whitespace-nowrap ml-1">Brandon Inc.</p>
     </div>
   );
 }
