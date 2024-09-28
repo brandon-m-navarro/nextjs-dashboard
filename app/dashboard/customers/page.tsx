068cd5bc4d64086a583ae22cb5470a10d284278d
brandon-m-navarro <brandon.m.navarro@gmail.com>
2024-09-28 00:08:45 -0400

    Implemented Customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 import Table from '@/app/ui/customers/table';
 import { TableRowSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Customers',
@@ -18,7 +20,16 @@ export default async function Page({
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   return (
+
     <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+
       <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
         <Table query={query} /*currentPage={currentPage}*/ />
       </Suspense>
