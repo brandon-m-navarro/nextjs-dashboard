3a2637aa1d5a4ebed653ffeb23e1477287c37776
Brandon <brandon.m.navarro@gmail.com>
2025-12-15 01:19:58 -0500

    Added redirect back to / on successful signout

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client";
 import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function SideNav() {
+  const router = useRouter();
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -21,7 +23,13 @@ export default function SideNav() {
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
           action={async () => {
-            authClient.signOut();
+            await authClient.signOut({
+              fetchOptions: {
+                onSuccess: () => {
+                  router.push("/"); // redirect landing page
+                },
+              },
+            });
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
